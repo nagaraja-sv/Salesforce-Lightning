ad05f6b37c0f0f1899f521de132b6a0f85f0bfe9
nagaraja-sv <venkatanagarajas@gmail.com>
2019-12-02 10:50:24 +0530

    adding the different filters to the search box

--- Property Mangement/propertyFilter/controller.js
+++ Property Mangement/propertyFilter/controller.js
@@ -1,12 +1,24 @@
 ({
 initFilter : function(component, event, helper) {
         var locationOption =[{label:'ALL',Value:'ALL'},
-                             {label:'Banglore',Value:'Banglore'},
-                            {label:'Hyderabad',Value:'Hyderabad'},
-                             {label:'Mumbai',Value:'Mumbai'},
-                             {label:'Pune',Value:'Pune'},
-                             {label:'Bhubaneswar',Value:'Bhubaneswar'}
-                            ];
+                                {label:'Banglore',Value:'Banglore'},
+                                {label:'Hyderabad',Value:'Hyderabad'},
+                                {label:'Mumbai',Value:'Mumbai'},
+                                {label:'Pune',Value:'Pune'},
+                                {label:'Bhubaneswar',Value:'Bhubaneswar'}
+                                ];
         component.set('v.locationOptions',locationOption);
+        var noOfBedRoomOptions = [{label:'ALL',value:'ALL'},
+                                        {label:'1',value:'1'},
+                                        {label:'2',value:'2'},
+                                        {label:'3',value:'3'},
+                                        {label:'4',value:'4'}];
+        component.set('v.noOfBedRoomOptions',noOfBedRoomOptions);
+                var noOfBathRoomOptions = [{label:'ALL',value:'ALL'},
+                                        {label:'1',value:'1'},
+                                        {label:'2',value:'2'},
+                                        {label:'3',value:'3'},
+                                        {label:'4',value:'4'}];
+        component.set('v.noOfBathRoomOptions',noOfBathRoomOptions);
 }
 })
